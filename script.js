b74687bbafb14ad6f175c60bb5dd80f60ea0c1a4
Vijay2004 <vijay909232@gmail.com>
2025-02-04 20:10:49 +0530

    All error correction

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function togglePopup() {
     const overlay = document.getElementById('popupOverlay');
     overlay.classList.toggle('show');
@@ -7,10 +8,9 @@ function register_togglePopup() {
     const r_overlay = document.getElementById('r-popupOverlay');
     r_overlay.classList.toggle('show');
 }
-
 const menuToggle = document.getElementById('menuToggle');
-const menu = document.getElementById('menu');
+        const menu = document.getElementById('menu');
 
-menuToggle.addEventListener('click', function () {
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+        menuToggle.addEventListener('click', function () {
+            menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 });
